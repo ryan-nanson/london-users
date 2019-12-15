9114b2816438dcf6946f79255b1f3cb77d2b19ee
ryan.nanson <ryan.nanson@engineering.digital.dwp.gov.uk>
2019-12-15 12:47:03 +0000

    Test users within radius of point returned

--- src/test/java/com/ryan/londonusers/service/UsersWithinRadiusServiceTest.java
+++ src/test/java/com/ryan/londonusers/service/UsersWithinRadiusServiceTest.java
@@ -58,6 +58,39 @@ public class UsersWithinRadiusServiceTest {
 
     assertThat("If distance between user and point is less than radius, user is returned",
         actualUsers, is(expectedUsers));
+  }
 
+  @Test
+  public void givenTheQueenReturnedAndLondonLongLatGiven_GetUsersWithinRadius_ReturnsTheQueen() {
+
+    // User with long, lat of Buckingham Palace
+    User theQueen = new User(
+        1,
+        "Elizabeth",
+        "Windsor",
+        "liz_windsor@bucks.com",
+        "0.0.0.0",
+        51.501401,
+        0.142159);
+
+    List<User> expectedUsers = new ArrayList<>();
+    expectedUsers.add(theQueen);
+
+    User[] users = new User[1];
+    users[0] = theQueen;
+
+    ResponseEntity<User[]> responseEntity = new ResponseEntity<>(users, HttpStatus.OK);
+
+    when(restTemplate.exchange("http://bpdts-test-app.herokuapp.com/users", HttpMethod.GET, null, User[].class))
+        .thenReturn(responseEntity);
+
+    // Call on London Long Lat with radius of 50 miles (in metres)
+    List<User> actualUsers = usersWithinRadiusService.getUsersWithinRadius(51.507222, -0.1275, 80467.2);
+
+    verify(restTemplate, times(1))
+        .exchange("http://bpdts-test-app.herokuapp.com/users", HttpMethod.GET, null, User[].class);
+
+    assertThat("If London long lat is given and the queen is the only user, she should be returned",
+        actualUsers, is(expectedUsers));
   }
 }
