8aef17f7fa29d684823e8427df0fc84c96c39a54
ryan.nanson <ryan.nanson@engineering.digital.dwp.gov.uk>
2019-12-18 11:30:16 +0000

    add full end to end test

--- src/integration-test/java/com/ryan/londonusers/LondonUsersIT.java
+++ src/integration-test/java/com/ryan/londonusers/LondonUsersIT.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {LondonUsersApplication.class})
+@ActiveProfiles("IT")
 @WebAppConfiguration
 public class LondonUsersIT {
 
